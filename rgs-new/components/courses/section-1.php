37b140745fe06c35ab6f058c5509ca65998cc2b5
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-01-26 12:04:23 +0530

    modified:   rgs-new/assets/css/styles.css new file:   rgs-new/assets/webp/bpharm-icon.webp new file:   rgs-new/assets/webp/dpharm-icon.webp new file:   rgs-new/components/courses/section-1.php new file:   rgs-new/components/courses/section-header.php modified:   rgs-new/courses.php

---
+++ rgs-new/components/courses/section-1.php
@@ -0,0 +1,35 @@
+<div class="container-fluid">
+    <div class="courses-section-1">
+        <a href="#" class="col-md-6 courses-section-1-txt" id="txt-left">
+            <div class="courses-section-1-img">
+                <img src="assets/webp/bpharm-icon.webp" alt="">
+            </div>
+            <h2>BACHELORS OF PHARMACY</h2>
+            <p>The Bachelor of Pharmacy degree is popularly known as B-Pharm in India. It is a four-year program
+                with
+                both annual and semester schemes available. To be eligible, one must pass with at least 50% marks in
+                10
+                + 2 (or an equivalent examination) with physics, chemistry, biology/ biotechnology/Maths as one of
+                the
+                subjects. Both PCB and PCM combinations are eligible.
+            </p>
+
+        </a>
+        <a href="#" class="col-md-6 courses-section-1-txt" id="txt-right">
+            <div class="courses-section-1-img">
+                <img src="assets/webp/dpharm-icon.webp" alt="">
+            </div>
+            <h2>DIPLOMA IN PHARMACY</h2>
+            <p>Diploma in Pharmacy is a short-range course for aspirants who want to start their career in the field
+                of
+                pharmacy. Instead of doing long term courses in pharmacy domain, D. Pharmacy course is the ideal
+                choice
+                for the student who wants a quick start in their pharmacy career. Pursuing D. Pharm can be a good
+                beginning to understand the fundamentals of pharmacy programme and then pursue advanced courses like
+                B.
+                Pharm & Pharm.D.
+            </p>
+
+        </a>
+    </div>
+</div>
---
+++ rgs-new/components/courses/section-header.php
@@ -0,0 +1,7 @@
+<div class="who-we-are-section-header-img-container">
+    <h1>COURSES</h1>
+    <div class="who-we-are-inner-link">
+        <a href="index.php">Home | </a>
+        <p> Courses</p>
+    </div>
+</div>
