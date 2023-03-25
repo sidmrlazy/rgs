7e30b677402b87f3167a72393862fe697bf2c51d
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-03-25 12:59:48 +0530

    modified:   rgs-new/controller/assets/admin-styles.css modified:   rgs-new/controller/dashboard/section-2.php deleted:    rgs-new/controller/includes/footer-admin.php modified:   rgs-new/controller/index-admin.php

--- rgs-new/controller/index-admin.php
+++ rgs-new/controller/index-admin.php
@@ -1,5 +1,6 @@
 <?php include('includes/header-admin.php') ?>
 <?php include('navbar/navbar-admin.php') ?>
 <?php include('dashboard/section-1.php') ?>
-<?php include('dashboard/section-2.php') ?>
+<?php include('dashboard/section-2.php')
+?>
 <?php include('includes/footer.php') ?>
