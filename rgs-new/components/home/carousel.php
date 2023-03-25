fb7bca94ae91448986c7ec00dca23d9378cc5d8d
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-03-25 14:00:01 +0530

    modified:   rgs-new/components/home/carousel.php

--- rgs-new/components/home/carousel.php
+++ rgs-new/components/home/carousel.php
@@ -8,15 +8,15 @@
             aria-label="Slide 3"></button>
     </div>
     <div class="carousel-inner">
-        <div class="carousel-item active">
+        <div class="carousel-item active" data-bs-interval="3000">
             <img src="assets/slider-images/homepage-slider-1.svg" class="d-block w-100"
                 alt="rgs-college-homepage-slider">
         </div>
-        <div class="carousel-item">
+        <div class="carousel-item" data-bs-interval="3000">
             <img src="assets/slider-images/homepage-slider-2.svg" class="d-block w-100"
                 alt="rgs-college-homepage-slider">
         </div>
-        <div class="carousel-item">
+        <div class="carousel-item" data-bs-interval="3000">
             <img src="assets/slider-images/homepage-slider-3.svg" class="d-block w-100"
                 alt="rgs-college-homepage-slider">
         </div>
