15bdf31d611e922fa8464626b979414d9dbd45c7
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-03-27 08:52:04 +0530

    modified:   includes/Footer.php

--- includes/Footer.php
+++ includes/Footer.php
@@ -70,13 +70,11 @@
 
 <!-- =========== DEVELOPER FOOTER =========== -->
 <div class="developer-footer">
-    <p>&#169; RGS College of Pharmacy | All Rights Reserved | Designed & Developed by <span><a
-                href="https://www.onlynus.com" target="_blank" rel="noopener noreferrer">Onlyn</a></span></p>
+    <p>&#169; RGS College of Pharmacy | All Rights Reserved | Designed & Developed by <span><a href="https://www.onlynus.com" target="_blank">Onlyn</a></span></p>
 </div>
 
 <!-- =========== BOOTSTRAP =========== -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
 </script>
 
 <!-- =========== IONICONS =========== -->
