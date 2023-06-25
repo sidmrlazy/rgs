8cac327e772fad43092dc9892ec9117c19c02d39
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-06-25 11:55:23 +0530

    modified:   assets/css/styles.css new file:   assets/icon/why-us-icon-1.png new file:   assets/icon/why-us-icon-2.png modified:   components/home/section-5.php

--- components/home/section-5.php
+++ components/home/section-5.php
@@ -1,4 +1,26 @@
 <div class="container home-section-5">
+    <h3>WHY US</h3>
+    <p>Why choose RGS College of Pharmacy?</p>
+
+    <div class="home-section-5-card-row">
+        <div class="home-section-5-card">
+            <img src="assets/icon/why-us-icon-1.png" alt="">
+            <h4>On Campus Facility</h4>
+            <p>Well-equipped Labs, Library, Practical Rooms enable our students to master their concepts and
+                become credible additions to the Pharma Sector.</p>
+        </div>
+        <div class="home-section-5-card">
+            <img src="assets/icon/why-us-icon-2.png" alt="">
+            <h4>Our Values</h4>
+            <p>Academics, Discipline, Dedication & Determination are the pillars of strength at RGS. We focus on
+                inculcating essential values like constructive competition, honesty & dedication towards
+                profession in our students.</p>
+        </div>
+    </div>
+</div>
+
+
+<!-- <div class="container home-section-5">
     <h3>Why RGS College of Pharmacy?</h3>
 
     <div class="home-section-5-row">
@@ -21,4 +43,4 @@
             <img src="assets/images/home-section-5-img.png" alt="">
         </div>
     </div>
-</div>
+</div> -->
