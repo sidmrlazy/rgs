4399eef2c075ad6cff5cd7de18293d848d12d271
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-08-10 12:54:28 +0530

    modified:   Events.php modified:   assets/css/styles.css

--- Events.php
+++ Events.php
@@ -6,7 +6,7 @@ include('includes/navbar.php'); ?>
     <div class="events-section-7-grid">
         <?php
         require('includes/db.php');
-        $fetch_album = "SELECT * FROM `event`";
+        $fetch_album = "SELECT * FROM `event` ORDER BY `event_date` DESC";
         $fetch_album_r = mysqli_query($connection, $fetch_album);
         while ($row = mysqli_fetch_assoc($fetch_album_r)) {
             $event_id = $row['event_id'];
@@ -16,15 +16,15 @@ include('includes/navbar.php'); ?>
             $event_date = $row['event_date'];
 
         ?>
-        <form action="ind-album.php" method="POST" class="events-section-7-card">
-            <input type="text" name="event_id" value="<?php echo $event_id ?>" hidden>
-            <button type="submit" name="open" class="events-section-7-btn">
-                <img src="<?php echo $event_album_img ?>" alt="">
-                <h3><?php echo $event_name ?></h3>
-                <p><?php echo $event_det ?></p>
-                <p><strong><?php echo $event_date ?></strong></p>
-            </button>
-        </form>
+            <form action="ind-album.php" method="POST" class="events-section-7-card">
+                <input type="text" name="event_id" value="<?php echo $event_id ?>" hidden>
+                <button type="submit" name="open" class="events-section-7-btn">
+                    <img src="<?php echo $event_album_img ?>" alt="">
+                    <h3><?php echo $event_name ?></h3>
+                    <p class="ellipses"><?php echo $event_det ?></p>
+                    <p><strong><?php echo $event_date ?></strong></p>
+                </button>
+            </form>
         <?php } ?>
     </div>
 </div>
