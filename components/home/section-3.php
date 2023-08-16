199f82643308daff47458e89d1366f4e31f71caa
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-08-16 17:29:10 +0530

    modified:   components/home/section-3.php

--- components/home/section-3.php
+++ components/home/section-3.php
@@ -5,18 +5,15 @@
                 <div class="carousel-inner">
                     <div class="carousel-item active">
                         <div class="home-section-3-slider-img">
-                            <img src="assets/images/home-slider-2.png" class="d-block w-100"
-                                alt="RGS College: Shaping Successful Pharmacy Careers">
+                            <img src="assets/images/home-slider-2.png" class="d-block w-100" alt="RGS College: Shaping Successful Pharmacy Careers">
                         </div>
                     </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#section3Carousel"
-                    data-bs-slide="prev">
+                <button class="carousel-control-prev" type="button" data-bs-target="#section3Carousel" data-bs-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Previous</span>
                 </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#section3Carousel"
-                    data-bs-slide="next">
+                <button class="carousel-control-next" type="button" data-bs-target="#section3Carousel" data-bs-slide="next">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Next</span>
                 </button>
@@ -28,7 +25,7 @@
             <p>RGS College of Pharmacy opened its gates to aspiring pharma students in the year 2019. The foundations of
                 the College were laid on the belief that students anywhere and everywhere should have access to quality
                 education and be provided with superior infrastructure and campus life in their own State...</p>
-            <a href="who-we-are.php">Read More</a>
+            <a href="who-we-are">Read More</a>
         </div>
     </div>
 </div>
