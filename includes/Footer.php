a900cdc7fd4b9cc17db51e5b131b0123e0185801
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-08-16 17:43:46 +0530

    modified:   includes/Footer.php modified:   includes/navbar.php

--- includes/Footer.php
+++ includes/Footer.php
@@ -30,17 +30,12 @@
 </div>
 
 <!-- =========== BOOTSTRAP =========== -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
 </script>
 
 <!-- =========== IONICONS =========== -->
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-
-
-<!-- =========== ADDTHIS =========== -->
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-6427c03c3106ef46"></script>
 
 </body>
 
