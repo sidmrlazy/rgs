8705972fcb19b68df333b599a23c67cbbf1e1d48
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-08-16 17:47:09 +0530

    deleted:    .htaccess modified:   components/home/homepage-connect.php modified:   index.php

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 $title = "Top Pharmacy College in Lucknow | RGS College of Pharmacy";
 include('includes/header.php') ?>
 <?php include('includes/navbar.php') ?>
-<?php // include('components/home/pop-up-modal.php') 
-?>
 <?php include('components/home/section-1.php') ?>
 <?php include('components/home/section-2.php') ?>
 <?php include('components/home/section-3.php') ?>
@@ -11,8 +9,5 @@ include('includes/header.php') ?>
 <?php include('components/home/section-7.php') ?>
 <?php include('components/home/section-5.php') ?>
 <?php include('components/home/section-6.php') ?>
-
 <?php include('components/home/homepage-connect.php') ?>
-<?php //include('components/home/events-brief.php') 
-?>
 <?php include('includes/Footer.php') ?>
