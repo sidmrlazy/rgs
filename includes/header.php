bdf0975e056a3f11be81fd9054e145ff30322484
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-08-16 18:11:51 +0530

    modified:   includes/header.php

--- includes/header.php
+++ includes/header.php
@@ -37,6 +37,19 @@
 </head>
 
 <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-21K21KK3PD"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-21K21KK3PD');
+</script>
+
+<!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-315G8JP6FJ"></script>
 <script>
     window.dataLayer = window.dataLayer || [];
