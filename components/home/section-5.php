aea1fd4aa4ecf2719a3626eb203863f253dd22ef
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-08-16 17:18:19 +0530

    modified:   assets/css/styles.css modified:   components/home/section-5.php

--- components/home/section-5.php
+++ components/home/section-5.php
@@ -3,13 +3,13 @@
     <p>Why choose RGS College of Pharmacy?</p>
 
     <div class="home-section-5-card-row">
-        <div class="home-section-5-card">
+        <div class="home-section-5-card w-100">
             <img src="assets/icon/why-us-icon-1.png" alt="Top Choice for Pharmacy Education: RGS College of Pharmacy">
             <h4>On Campus Facility</h4>
             <p>Well-equipped Labs, Library, Practical Rooms enable our students to master their concepts and
                 become credible additions to the Pharma Sector.</p>
         </div>
-        <div class="home-section-5-card">
+        <div class="home-section-5-card w-100">
             <img src="assets/icon/why-us-icon-2.png" alt="Quality Education at RGS Pharmacy College, Lucknow">
             <h4>Our Values</h4>
             <p>Academics, Discipline, Dedication & Determination are the pillars of strength at RGS. We focus on
