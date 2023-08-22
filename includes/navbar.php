68d4421c23a6fe830f37ab76f071f8fe71109877
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-08-22 09:58:36 +0530

    modified:   assets/css/styles.css modified:   includes/navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -21,6 +21,18 @@
                 <strong>Affiliated by PCI</strong>
             </p>
         </div>
+
+        <div class="top-nav-item">
+            <a href="https://www.facebook.com/RGSCollegeOfPharmacy" class="top-nav-link-media" target="_blank">
+                <ion-icon name="logo-facebook"></ion-icon>
+            </a>
+        </div>
+
+        <div class="top-nav-item">
+            <a href="https://www.instagram.com/rgscp/" class="top-nav-link-media" target="_blank">
+                <ion-icon name="logo-instagram"></ion-icon>
+            </a>
+        </div>
     </div>
 
 </div>
@@ -31,38 +43,39 @@
         <a class="navbar-brand" href="index.php">
             <img src="assets/logo/rgs-logo.webp" alt="RGS College of Pharmacy: Shaping Tomorrow's Pharmacists">
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mx-auto me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                     <?php if ($title === "Top College of Pharmacy in Lucknow | RGS College of Pharmacy") {  ?>
-                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
                     <?php } else { ?>
-                        <a class="nav-link" aria-current="page" href="index.php">Home</a>
+                    <a class="nav-link" aria-current="page" href="index.php">Home</a>
                     <?php } ?>
                 </li>
                 <li class="nav-item">
                     <?php if ($title === "Who We Are | RGS College of Pharmacy") { ?>
-                        <a class="nav-link active" href="who-we-are.php">Who we are</a>
+                    <a class="nav-link active" href="who-we-are.php">Who we are</a>
                     <?php } else { ?>
-                        <a class="nav-link" href="who-we-are.php">Who we are</a>
+                    <a class="nav-link" href="who-we-are.php">Who we are</a>
                     <?php } ?>
                 </li>
 
                 <li class="nav-item">
                     <?php if ($title === "Admissions | RGS College of Pharmacy") { ?>
-                        <a class="nav-link active" href="admissions.php">Admissions</a>
+                    <a class="nav-link active" href="admissions.php">Admissions</a>
                     <?php } else { ?>
-                        <a class="nav-link" href="admissions.php">Admissions</a>
+                    <a class="nav-link" href="admissions.php">Admissions</a>
                     <?php } ?>
                 </li>
                 <li class="nav-item">
                     <?php if ($title === "Events | RGS College of Pharmacy") { ?>
-                        <a class="nav-link active" href="events.php">Events</a>
+                    <a class="nav-link active" href="events.php">Events</a>
                     <?php } else { ?>
-                        <a class="nav-link" href="events.php">Events</a>
+                    <a class="nav-link" href="events.php">Events</a>
                     <?php } ?>
                 </li>
                 <!-- <li class="nav-item">
@@ -87,13 +100,13 @@
 
                 if ($fetch_data_count > 0) {
                 ?>
-                    <li class="nav-item">
-                        <?php if ($title === "Student Corner | RGS College of Pharmacy") { ?>
-                            <a class="nav-link active" href="student-corner.php">Student Corner</a>
-                        <?php } else { ?>
-                            <a class="nav-link" href="student-corner.php">Student Corner</a>
-                        <?php } ?>
-                    </li>
+                <li class="nav-item">
+                    <?php if ($title === "Student Corner | RGS College of Pharmacy") { ?>
+                    <a class="nav-link active" href="student-corner.php">Student Corner</a>
+                    <?php } else { ?>
+                    <a class="nav-link" href="student-corner.php">Student Corner</a>
+                    <?php } ?>
+                </li>
                 <?php } ?>
                 <!-- <li class="nav-item">
                     <?php if ($title === "Resources | RGS College of Pharmacy") { ?>
@@ -105,13 +118,15 @@
 
                 <li class="nav-item dropdown">
                     <?php if ($title === "Academics | RGS College of Pharmacy" || $title === "Calendar | RGS College of Pharmacy") { ?>
-                        <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Academics
-                        </a>
+                    <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Academics
+                    </a>
                     <?php } else { ?>
-                        <a class="nav-link  dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Academics
-                        </a>
+                    <a class="nav-link  dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Academics
+                    </a>
                     <?php } ?>
                     <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="courses.php">Courses</a></li>
@@ -130,9 +145,9 @@
                 </li> -->
                 <li class="nav-item">
                     <?php if ($title === "Contact Us | RGS College of Pharmacy") { ?>
-                        <a class="nav-link active" href="contact.php">Contact</a>
+                    <a class="nav-link active" href="contact.php">Contact</a>
                     <?php } else { ?>
-                        <a class="nav-link" href="contact.php">Contact</a>
+                    <a class="nav-link" href="contact.php">Contact</a>
                     <?php } ?>
                 </li>
             </ul>
